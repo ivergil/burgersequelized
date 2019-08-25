3a30057e819d6614bc679da74ca7421ceed25322
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-25 14:52:26 -0600

    controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,38 +1,50 @@
 var express = require("express");
-var burgerORM = require("../models/burger.js");
+var Burger = require("../models/burger.js");
 
 var router = express.Router();
+
+var db = require("../models");
 //create routers and export them
 
 //select all burgers from the db and render them
 router.get("/", function(req, res) {
-    burgerORM.selectAll(function(data) {
-        res.render("index", {burger:data});
+    db.Burgers.findAll().then(function(dbBurger) {
+        var burgerObj = {
+            burgers: dbBurger
+        };
+        res.render("index",burgerObj);
  });
 });
 
 //post a new burger to the db
 router.post("/", function(req, res) {
-    burgerORM.insertOne(req.body.burger, function(result) {
-        // console.log(result);
-        // console.log("entro por insertOne");
+    db.Burgers.create(req.body).then(function(dbBurger) {
+        //console.log(dbBurger);
         res.redirect("/");
   });
 });
 
 //update a burger's devoured status
 router.put("/:id", function(req,res) {
-    burgerORM.updateOne(req.params.id, function(result) {
-        // console.log(result);
-        // console.log("entro por updateOne");
+  console.log(req.body);
+    db.Burgers.update({
+      devoured: true
+    }, {
+      where: {
+          id: req.params.id
+      }
+    }).then(function(dbBurger) {
         res.redirect("/");
   });
 });
 
 //delete a burger from the database
 router.delete("/:id", function(req,res) {
-    burgerORM.deleteOne(req.params.id, function(result) {
-        // console.log("entro por deleteOne");
+    db.Burgers.destroy ({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbBurger) {
       res.redirect("/");
     });
 });
